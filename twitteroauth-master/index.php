801f9890c231496416a2b1e789298654c1b31e60
Kevin Doherty <kevindoherty@Kevins-MacBook-Air.local>
2017-09-13 10:26:32 -0400

    initial twitter api commit

---
+++ twitteroauth-master/index.php
@@ -0,0 +1,22 @@
+<?php
+//keys and tokens
+$consumer_key = '4T7eFElkLYhosV5jL3I4eDZb9';
+$consumer_secret = 'NYEnnzL2bwijUOaT5eIIefirZAU3jA6uCPJKnepcHAzmAMmII3';
+$access_token = '569220369-YoFpqLvwYQLj5NeG2scE050zNCoKuTUJSskpgEYP';
+$access_token_secret = 'iuhCxeBGqISI2b6nH4sKk96LA4DeM7lQCx8iVl2NWAs7Z';
+
+//Include library
+require "autoload.php";
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//Connect to API
+$connection = new TwitterOAuth ($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+$content = $connection->get("account/verify_credentials");
+
+//Create Tweets
+$new_status = $connection->post("statuses/update", ["status" => "This tweet sent via Twitter API."]);
+
+//Get Tweets
+$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+
+print_r($statuses);
